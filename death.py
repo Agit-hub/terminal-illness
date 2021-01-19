4836c6379e85d48250b2bed8edd09bb31df0929c
Kira Newton <k.i.newton@students.uu.nl>
2021-01-19 14:34:38 +0100

    changed profession to occupation

--- death.py
+++ death.py
@@ -5,6 +5,6 @@ with open('cause_of_death.json') as file:
     data = json.load(file)
 
 with open('cause_of_death.csv', 'w') as file:
-    file.write('Title; death_cause; Profession; Death Year; Death Place\n')
+    file.write('Title; death_cause; Occupation; Death Year; Death Place\n')
     for person in data:
-        file.write(f"{person['title']}​​​​;{person['ontology/deathCause_label']}​​​​;{person.get('ontology/profession_label')}​​​​;{person.get('ontology/deathYear')}​​​​;{person.get('ontology/deathPlace_label')}​​​​\n")
+        file.write(f"{person['title']}​​​​;{person['ontology/deathCause_label']}​​​​;{person.get('ontology/occupation_label')}​​​​;{person.get('ontology/deathYear')}​​​​;{person.get('ontology/deathPlace_label')}​​​​\n")
