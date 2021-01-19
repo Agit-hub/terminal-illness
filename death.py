89dabe9954594070c4423ea0699066c9179750b9
Kira Newton <k.i.newton@students.uu.nl>
2021-01-19 10:32:02 +0100

    script for csv file

---
+++ death.py
@@ -0,0 +1,10 @@
+import json 
+import csv
+
+with open('cause_of_death.json') as file:
+    data = json.load(file)
+
+with open('cause_of_death.csv', 'w') as file:
+    file.write('Title, Cause of Death, Profession, Death Year, Death Place\n')
+    for person in data:
+        file.write(f"{person['title']}​​​​,{person['ontology/deathCause_label']}​​​​,{person.get('ontology/profession_label')}​​​​,{person.get('deathYear')}​​​​,{person.get('deathPlace')}​​​​\n")
