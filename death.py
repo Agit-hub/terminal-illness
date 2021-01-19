110fedb7a6e71e1976a4efbdc94329d56a1f93d7
Kira Newton <k.i.newton@students.uu.nl>
2021-01-19 14:41:54 +0100

    changed titles

--- death.py
+++ death.py
@@ -5,6 +5,6 @@ with open('cause_of_death.json') as file:
     data = json.load(file)
 
 with open('cause_of_death.csv', 'w') as file:
-    file.write('Title; death_cause; Occupation; Death Year; Death Place\n')
+    file.write('Title; death_cause; Occupation; Death_Year; Death_Place\n')
     for person in data:
         file.write(f"{person['title']}​​​​;{person['ontology/deathCause_label']}​​​​;{person.get('ontology/occupation_label')}​​​​;{person.get('ontology/deathYear')}​​​​;{person.get('ontology/deathPlace_label')}​​​​\n")
