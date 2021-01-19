6219ed66d195e20a086df792734fb595c8a05b0e
Kira Newton <k.i.newton@students.uu.nl>
2021-01-19 13:53:04 +0100

    change to semi colon

--- death.py
+++ death.py
@@ -7,4 +7,4 @@ with open('cause_of_death.json') as file:
 with open('cause_of_death.csv', 'w') as file:
     file.write('Title, Cause of Death, Profession, Death Year, Death Place\n')
     for person in data:
-        file.write(f"{person['title']}​​​​,{person['ontology/deathCause_label']}​​​​,{person.get('ontology/profession_label')}​​​​,{person.get('deathYear')}​​​​,{person.get('deathPlace')}​​​​\n")
+        file.write(f"{person['title']}​​​​;{person['ontology/deathCause_label']}​​​​;{person.get('ontology/profession_label')}​​​​;{person.get('deathYear')}​​​​;{person.get('deathPlace')}​​​​\n")
