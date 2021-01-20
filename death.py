a34aa57026f28b68672d84e16e154ad50c3a0ebb
Kira Newton <k.i.newton@students.uu.nl>
2021-01-20 12:20:06 +0100

    added birth year so that age can be calculated

--- death.py
+++ death.py
@@ -5,6 +5,6 @@ with open('cause_of_death.json') as file:
     data = json.load(file)
 
 with open('cause_of_death.csv', 'w') as file:
-    file.write('Title; death_cause; Occupation; Death_Year; Death_Place\n')
+    file.write('Title; death_cause; Occupation; Death_Year; Birth_Year; Death_Place\n')
     for person in data:
-        file.write(f"{person['title']}​​​​;{person['ontology/deathCause_label']}​​​​;{person.get('ontology/occupation_label')}​​​​;{person.get('ontology/deathYear')}​​​​;{person.get('ontology/deathPlace_label')}​​​​\n")
+        file.write(f"{person['title']}​​​​;{person['ontology/deathCause_label']}​​​​;{person.get('ontology/occupation_label')}​​​​;{person.get('ontology/deathYear')}​​​​;{person.get('ontology/birthYear')}​​​​;{person.get('ontology/deathPlace_label')}​​​​\n")
